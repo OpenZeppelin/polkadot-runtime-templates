40da7151381d877beaa683880e08c8ecd73c0ea6
4meta5 <asinghchrony@protonmail.com>
2023-11-28 16:17:27 -0500

    clippy fmt

--- runtime/tests/common/mod.rs
+++ runtime/tests/common/mod.rs
@@ -2,10 +2,7 @@
 use frame_support::weights::Weight;
 use parachain_template_runtime::{BuildStorage, Runtime, System};
 
-pub fn run_with_system_weight<F>(w: Weight, mut assertions: F)
-where
-    F: FnMut() -> (),
-{
+pub fn run_with_system_weight<F: FnMut()>(w: Weight, mut assertions: F) {
     let mut t: sp_io::TestExternalities =
         frame_system::GenesisConfig::<Runtime>::default().build_storage().unwrap().into();
     t.execute_with(|| {
