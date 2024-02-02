3236595ec73a26a12e5663a594be5317c76f6c3a
Nikita Khateev <nikita.khateev@gmail.com>
2024-02-02 19:28:05 +0400

    Fuzzer Integration

---
+++ template-fuzzer/run-fuzzer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rustup target add wasm32-unknown-unknown
+cargo install ziggy cargo-afl honggfuzz grcov
+AFL_SKIP_CPUFREQ=true AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=true cargo ziggy fuzz -t 20 -j 5
