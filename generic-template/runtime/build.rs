0c928f6d15ad6e9060d4cd7da1302567b0d14f40
4meta5 <asinghchrony@protonmail.com>
2024-05-31 16:50:51 -0400

    fmt

--- generic-template/runtime/build.rs
+++ generic-template/runtime/build.rs
@@ -1,13 +1,13 @@
 #[cfg(all(not(feature = "metadata-hash"), feature = "std"))]
 fn main() {
-	substrate_wasm_builder::WasmBuilder::build_using_defaults();
+    substrate_wasm_builder::WasmBuilder::build_using_defaults();
 }
 
 #[cfg(all(feature = "metadata-hash", feature = "std"))]
 fn main() {
-	substrate_wasm_builder::WasmBuilder::init_with_defaults()
-		.enable_metadata_hash("UNIT", 12)
-		.build();
+    substrate_wasm_builder::WasmBuilder::init_with_defaults()
+        .enable_metadata_hash("UNIT", 12)
+        .build();
 }
 
 /// The wasm builder is deactivated when compiling
