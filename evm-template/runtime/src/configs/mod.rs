bdf7667229c5cba50f6f02025921a9986fce29bd
4meta5 <asinghchrony@protonmail.com>
2024-06-06 16:17:51 -0400

    fixes

--- evm-template/runtime/src/configs/mod.rs
+++ evm-template/runtime/src/configs/mod.rs
@@ -8,7 +8,7 @@ use cumulus_pallet_parachain_system::RelayNumberStrictlyIncreases;
 use cumulus_primitives_core::{AggregateMessageOrigin, ParaId};
 use frame_support::{
     derive_impl,
-    dispatch::DispatchClass,
+    dispatch::{DispatchClass, GetDispatchInfo},
     parameter_types,
     traits::{
         AsEnsureOriginWithArg, ConstU32, ConstU64, Contains, EitherOfDiverse, FindAuthor,
@@ -59,11 +59,11 @@ use crate::{
     },
     opaque,
     types::{
-        AccountId, AssetId, Assets, Balance, Block, BlockNumber, CollatorSelectionUpdateOrigin,
+        AccountId, AssetId, Balance, Block, BlockNumber, CollatorSelectionUpdateOrigin,
         ConsensusHook, Hash, Nonce,
     },
     weights::{BlockExecutionWeight, ExtrinsicBaseWeight, RocksDbWeight},
-    AssetManager, Aura, Balances, BaseFee, CollatorSelection, EVMChainId, MessageQueue,
+    AssetManager, Assets, Aura, Balances, BaseFee, CollatorSelection, EVMChainId, MessageQueue,
     OpenZeppelinPrecompiles, OriginCaller, PalletInfo, ParachainSystem, Preimage, Runtime,
     RuntimeCall, RuntimeEvent, RuntimeFreezeReason, RuntimeHoldReason, RuntimeOrigin, RuntimeTask,
     Session, SessionKeys, System, Timestamp, Treasury, UncheckedExtrinsic, WeightToFee, XcmpQueue,
@@ -346,6 +346,63 @@ impl pallet_assets::Config for Runtime {
     type WeightInfo = pallet_assets::weights::SubstrateWeight<Runtime>;
 }
 
+// Our AssetType. For now we only handle Xcm Assets
+#[derive(Clone, Eq, Debug, PartialEq, Ord, PartialOrd, Encode, Decode, TypeInfo)]
+pub enum AssetType {
+    Xcm(xcm::v3::Location),
+}
+impl Default for AssetType {
+    fn default() -> Self {
+        Self::Xcm(xcm::v3::Location::here())
+    }
+}
+
+impl From<xcm::v3::Location> for AssetType {
+    fn from(location: xcm::v3::Location) -> Self {
+        Self::Xcm(location)
+    }
+}
+
+// This can be removed once we fully adopt xcm::v4 everywhere
+impl TryFrom<Location> for AssetType {
+    type Error = ();
+
+    fn try_from(location: Location) -> Result<Self, Self::Error> {
+        Ok(Self::Xcm(location.try_into()?))
+    }
+}
+
+impl Into<Option<xcm::v3::Location>> for AssetType {
+    fn into(self) -> Option<xcm::v3::Location> {
+        match self {
+            Self::Xcm(location) => Some(location),
+        }
+    }
+}
+
+impl Into<Option<Location>> for AssetType {
+    fn into(self) -> Option<Location> {
+        match self {
+            Self::Xcm(location) => xcm_builder::V4V3LocationConverter::convert_back(&location),
+        }
+    }
+}
+
+// Implementation on how to retrieve the AssetId from an AssetType
+// We take it
+impl From<AssetType> for AssetId {
+    fn from(asset: AssetType) -> AssetId {
+        match asset {
+            AssetType::Xcm(id) => {
+                let mut result: [u8; 16] = [0u8; 16];
+                let hash: H256 = id.using_encoded(<Runtime as frame_system::Config>::Hashing::hash);
+                result.copy_from_slice(&hash.as_fixed_bytes()[0..16]);
+                u128::from_le_bytes(result)
+            }
+        }
+    }
+}
+
 // We instruct how to register the Assets
 // In this case, we tell it to Create an Asset in pallet-assets
 pub struct AssetRegistrar;
@@ -416,7 +473,7 @@ impl pallet_asset_manager::Config for Runtime {
     type AssetRegistrarMetadata = AssetRegistrarMetadata;
     type Balance = Balance;
     type ForeignAssetModifierOrigin = EnsureRoot<AccountId>;
-    type ForeignAssetType = xcm_config::AssetType;
+    type ForeignAssetType = AssetType;
     type RuntimeEvent = RuntimeEvent;
     type WeightInfo = (); //FIXME: run & update
 }
