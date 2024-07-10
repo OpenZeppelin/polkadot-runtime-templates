60414ec1b3511585868b570dc3a2ff933015b95a
Nikita Khateev <nikita.khateev@openzeppelin.com>
2024-07-10 14:35:33 +0400

    fix comments

--- evm-template/rpc-testing/TestContract.sol
+++ evm-template/rpc-testing/TestContract.sol
@@ -1,10 +1,8 @@
 contract Storage {
     uint pos0;
-    mapping(address => uint) pos1;
 
     constructor() {
         pos0 = 1234;
-        pos1[msg.sender] = 5678;
     }
 
     // You can read from a state variable without sending a transaction.
