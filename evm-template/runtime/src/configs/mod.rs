061e1632f8a2ac854ca81aa94e2c5c70f3819ea2
Nikita Khateev <nikita.khateev@openzeppelin.com>
2024-07-15 13:26:53 +0400

    minor changes

--- evm-template/runtime/src/configs/mod.rs
+++ evm-template/runtime/src/configs/mod.rs
@@ -593,7 +593,7 @@ impl pallet_ethereum::Config for Runtime {
 
 parameter_types! {
     pub BlockGasLimit: U256 = U256::from(NORMAL_DISPATCH_RATIO * MAXIMUM_BLOCK_WEIGHT.ref_time() / WEIGHT_PER_GAS);
-    pub GasLimitPovSizeRatio: u64 = BlockGasLimit::get().as_u64().min(u64::MAX).saturating_div(cumulus_primitives_core::relay_chain::MAX_POV_SIZE as u64);
+    pub GasLimitPovSizeRatio: u64 = BlockGasLimit::get().min(u64::MAX.into()).low_u64().saturating_div(cumulus_primitives_core::relay_chain::MAX_POV_SIZE as u64);
     pub PrecompilesValue: OpenZeppelinPrecompiles<Runtime> = OpenZeppelinPrecompiles::<_>::new();
     pub WeightPerGas: Weight = Weight::from_parts(WEIGHT_PER_GAS, 0);
     pub SuicideQuickClearLimit: u32 = 0;
