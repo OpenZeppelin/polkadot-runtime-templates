e6fadaa9e4cfaf1a8efe952c29ef5b77e187e9cf
4meta5 <asinghchrony@protonmail.com>
2024-08-16 15:47:54 -0400

    compiles but not exactly what we want yet

--- generic-template/oz-wrappers/src/lib.rs
+++ generic-template/oz-wrappers/src/lib.rs
@@ -2,43 +2,60 @@
 
 use frame_support::{traits::Get, weights::constants::RocksDbWeight, Parameter};
 use frame_support_procedural::{derive_impl, inject_runtime_type, register_default_impl};
+use frame_system::limits::{BlockLength, BlockWeights};
 pub use oz_config::OzSystemConfig;
 use pallet_balances::AccountData;
 use parity_scale_codec::{Decode, Encode, EncodeLike, FullCodec, MaxEncodedLen};
 use primitives::{generic::*, Balance, BlockNumber, Hash, Moment};
 use scale_info::{prelude::fmt::Debug, TypeInfo};
-use sp_runtime::traits::{MaybeDisplay, MaybeSerializeDeserialize, Member};
+use sp_runtime::{
+    traits::{MaybeDisplay, MaybeSerializeDeserialize, Member},
+    Perbill,
+};
 
 #[derive(Clone, PartialEq, Eq)]
 pub struct OzSystem<Runtime>(core::marker::PhantomData<Runtime>);
 
-#[derive_impl(frame_system::config_preludes::ParaChainDefaultConfig as frame_system::DefaultConfig)]
+pub struct RuntimeBlockWeights<Runtime: OzSystemConfig>(core::marker::PhantomData<Runtime>);
+impl<Runtime: OzSystemConfig> Get<BlockWeights> for RuntimeBlockWeights<Runtime> {
+    fn get() -> BlockWeights {
+        BlockWeights::with_sensible_defaults(Runtime::BlockWeight::get(), Perbill::one())
+    }
+}
+
+pub struct RuntimeBlockLength<Runtime: OzSystemConfig>(core::marker::PhantomData<Runtime>);
+impl<Runtime: OzSystemConfig> Get<BlockLength> for RuntimeBlockLength<Runtime> {
+    fn get() -> BlockLength {
+        BlockLength::max(Runtime::BlockLength::get())
+    }
+}
+
 #[register_default_impl(OzSystem)]
-impl<Runtime: OzSystemConfig> frame_system::Config for OzSystem<Runtime> {
-    // configured by OzSystemConfig
+impl<Runtime: OzSystemConfig> frame_system::DefaultConfig for OzSystem<Runtime> {
     type AccountData = AccountData<Balance>;
     type AccountId = <Runtime as OzSystemConfig>::AccountId;
     // TODO: replace with NormalFilter
     type BaseCallFilter = frame_support::traits::Everything;
-    /// The block type.
-    type Block = Block<Self::RuntimeCall>;
     // TODO: replace with polkadot_runtime_common::BlockHashCount
     type BlockHashCount = frame_support::traits::ConstU32<256>;
-    // TODO: add these back
-    // type BlockLength = ();
-    // type BlockWeights = ();
+    type BlockLength = RuntimeBlockLength<Runtime>;
+    type BlockWeights = RuntimeBlockWeights<Runtime>;
     type DbWeight = RocksDbWeight;
     type Hash = Hash;
     type Hashing = sp_runtime::traits::BlakeTwo256;
     type Lookup = sp_runtime::traits::AccountIdLookup<Self::AccountId, ()>;
     type MaxConsumers = frame_support::traits::ConstU32<16>;
+    type MultiBlockMigrator = ();
     type Nonce = u32;
-    // The action to take on a Runtime Upgrade
-    // type OnSetCode = cumulus_pallet_parachain_system::ParachainSetCode<Self>;
-
-    // injected
+    type OnKilledAccount = ();
+    type OnNewAccount = ();
+    // TODO: set to cumulus_pallet_parachain_system::ParachainSetCode<Self>;
+    type OnSetCode = ();
     #[inject_runtime_type]
     type PalletInfo = ();
+    type PostInherents = ();
+    type PostTransactions = ();
+    type PreInherents = ();
     #[inject_runtime_type]
     type RuntimeCall = ();
     #[inject_runtime_type]
@@ -48,11 +65,15 @@ impl<Runtime: OzSystemConfig> frame_system::Config for OzSystem<Runtime> {
     #[inject_runtime_type]
     type RuntimeTask = ();
     type SS58Prefix = <Runtime as OzSystemConfig>::SS58Prefix;
+    type SingleBlockMigrations = ();
+    // TODO: set
+    type SystemWeightInfo = ();
     type Version = <Runtime as OzSystemConfig>::Version;
 }
 
 #[frame_support::pallet]
 pub mod oz_config {
+    use frame_support::pallet_prelude::*;
     use frame_system::pallet_prelude::*;
 
     use super::*;
@@ -69,6 +90,9 @@ pub mod oz_config {
             + MaxEncodedLen;
         type SS58Prefix: Get<u16>;
         type Version: Get<sp_version::RuntimeVersion>;
+        // TODO: remove and replace with constants?
+        type BlockWeight: Get<Weight>;
+        type BlockLength: Get<u32>;
         //type MinimumBalance: Get<Balance>;
     }
 
