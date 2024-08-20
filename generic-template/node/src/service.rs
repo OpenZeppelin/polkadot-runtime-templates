df9a097a6423d2b217cabd95529e59e2109aa47a
Ozgun Ozerk <ozgun@subspace.network>
2024-08-20 10:47:50 +0300

    generic template updated to stable2407-1

--- generic-template/node/src/service.rs
+++ generic-template/node/src/service.rs
@@ -28,7 +28,6 @@ use sc_client_api::Backend;
 use sc_consensus::ImportQueue;
 use sc_executor::{HeapAllocStrategy, WasmExecutor, DEFAULT_HEAP_ALLOC_STRATEGY};
 use sc_network::NetworkBlock;
-use sc_network_sync::SyncingService;
 use sc_service::{Configuration, PartialComponents, TFullBackend, TFullClient, TaskManager};
 use sc_telemetry::{Telemetry, TelemetryHandle, TelemetryWorker, TelemetryWorkerHandle};
 use sc_transaction_pool_api::OffchainTransactionPoolFactory;
@@ -303,7 +302,6 @@ async fn start_node_impl(
             &task_manager,
             relay_chain_interface.clone(),
             transaction_pool,
-            sync_service.clone(),
             params.keystore_container.keystore(),
             relay_chain_slot_duration,
             para_id,
@@ -354,7 +352,6 @@ fn start_consensus(
     task_manager: &TaskManager,
     relay_chain_interface: Arc<dyn RelayChainInterface>,
     transaction_pool: Arc<sc_transaction_pool::FullPool<Block, ParachainClient>>,
-    sync_oracle: Arc<SyncingService<Block>>,
     keystore: KeystorePtr,
     relay_chain_slot_duration: Duration,
     para_id: ParaId,
@@ -398,7 +395,6 @@ fn start_consensus(
         code_hash_provider: move |block_hash| {
             client.code_at(block_hash).ok().map(ValidationCode).map(|c| c.hash())
         },
-        sync_oracle,
         keystore,
         collator_key,
         para_id,
@@ -419,12 +415,12 @@ fn start_consensus(
 
     #[cfg(not(feature = "async-backing"))]
     let fut =
-        basic_aura::run::<Block, sp_consensus_aura::sr25519::AuthorityPair, _, _, _, _, _, _, _>(
+        basic_aura::run::<Block, sp_consensus_aura::sr25519::AuthorityPair, _, _, _, _, _, _>(
             params,
         );
     #[cfg(feature = "async-backing")]
     let fut =
-        aura::run::<Block, sp_consensus_aura::sr25519::AuthorityPair, _, _, _, _, _, _, _, _, _>(
+        aura::run::<Block, sp_consensus_aura::sr25519::AuthorityPair, _, _, _, _, _, _, _, _>(
             params,
         );
     task_manager.spawn_essential_handle().spawn("aura", None, fut);
