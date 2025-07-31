2d55fac66bf8a0d3d7fede39e40114e805bd4af2
4meta5 <asinghchrony@protonmail.com>
2025-07-31 13:49:39 -0400

    break build but apply suggestions from @KitHat

--- evm-template/runtime/src/configs/governance/origins.rs
+++ evm-template/runtime/src/configs/governance/origins.rs
@@ -60,6 +60,7 @@ pub mod pallet_custom_origins {
                         r => Err(O::from(r)),
                     })
                 }
+                #[cfg(feature = "runtime-benchmarks")]
                 fn try_successful_origin() -> Result<O, ()> {
                     Ok(O::from(Origin::$name))
                 }
@@ -97,6 +98,7 @@ pub mod pallet_custom_origins {
                         r => Err(O::from(r)),
                     })
                 }
+                #[cfg(feature = "runtime-benchmarks")]
                 fn try_successful_origin() -> Result<O, ()> {
                     // By convention the more privileged origins go later, so for greatest chance
                     // of success, we want the last one.
